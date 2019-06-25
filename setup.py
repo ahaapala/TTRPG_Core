8e3fbb297a1ddbd5619a095ea98f31bc5838ec1c
Adam Haapala <adamhaapala@gmail.com>
2019-06-25 13:14:52 -0400

    Testing out a local commit to see about the linting

--- setup.py
+++ setup.py
@@ -2,10 +2,10 @@ from setuptools import setup
 
 setup(
    name='ttrpg_core',
-   version='0.0.1',
+   version='0.0.2',
    description='A module for exploring ttrpg systems',
    author='Adam Haapala',
    author_email='adamhaapala@yahoo.com',
-   packages=['ttrpg_core'],  #same as name
-   install_requires=['numpy', 'matplotlib'], #external packages as dependencies
+   packages=['ttrpg_core'],
+   install_requires=['numpy', 'matplotlib'],
 )
